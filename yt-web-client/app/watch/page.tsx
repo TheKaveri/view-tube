9d96033800948f839dbfdacbfb24a3ac4451bacc
TheKaveri <jimgv256@gmail.com>
2024-08-14 16:00:07 -0230

    added some CSS :(

--- yt-web-client/app/watch/page.tsx
+++ yt-web-client/app/watch/page.tsx
@@ -7,6 +7,7 @@ import styles from './page.module.css';
 import { getVideoUploader } from '../firebase/functions';
 
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 export interface User {
     displayName?: string;
@@ -16,13 +17,16 @@ export interface User {
 }
 
 function WatchPage() {
-    const videoPrefix = 'https://storage.googleapis.com/narmada-processed-videos/';
+    const resourcePrefix = 'https://storage.googleapis.com/narmada-processed-videos/';
     const videoSrc = useSearchParams().get('v');
 
     const [uploader, setUploader] = useState<User|null>(null);
 
     let res = videoSrc?.split(".")[0]
     res = res?.split("processed-")[1]
+
+    const thumbnailSrc = "thumbnail-" + res + ".png";
+    const videoName = res?.split("-")[2];
 
     useEffect(() => {
         const fetchUploader = async () => {
@@ -35,17 +39,24 @@ function WatchPage() {
     }, [res]);
 
     return (
-        <div>
-            {/* <h1>Watch Page</h1> */}
-            <video controls src={videoPrefix + videoSrc} className={styles.video}/>
-            {uploader && (
-                <div>
-                    <h2>Uploader Information</h2>
-                    <p>{JSON.stringify(uploader)}</p>
-                </div>
-            )}
+        <div className={styles.watchPageContainer}>
+            <video controls src={resourcePrefix + videoSrc} className={styles.video} poster={resourcePrefix + thumbnailSrc}/>
+            <h2>{videoName}</h2>
+            {uploader && <Uploader uploader={uploader}/>}
          </div>
     );
+}
+
+function Uploader({uploader}: {uploader: User}) {
+    return (
+        <div className={styles.uploaderContainer}>
+            <Image className={styles.uploaderIcon} src={(uploader.photoURL)? uploader.photoURL : "./thumbnail.png"} alt="user icon" title={uploader.displayName || ""} width={40} height={40}/>
+            <div className={styles.uploaderInfo}>
+                <span className={styles.displayName}>{uploader.displayName}</span>
+                <span className={styles.email}>{uploader.email}</span>
+            </div>
+        </div>
+    )   
 }
 
 export default function Watch() {
